1e51f6f80f2b86e6be2c56542792145e3c0cf775
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-06-29 19:35:56 -0600

    Learn contours?

---
+++ reading_images_video/contours.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('photos/cat.jpeg')
+cv.imshow('Cats', img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+cv.imshow('Blank', blank)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('Blue', blur)
+
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+cv.imshow('Thresh', thresh)
+
+contours, hierarchies = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+print(f'{len(contours)} contour(s) found!')
+
+cv.drawContours(blank, contours, -1, (0,0,255), 1)
+cv.imshow('Contours Drawn', blank)
+
+cv.waitKey(0)
